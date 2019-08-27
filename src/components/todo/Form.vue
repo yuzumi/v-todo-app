00cf09db3a4708aec0bfb8be7f4be94a6a5175d4
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-27 21:14:50 +0300

    Apply created components

--- src/components/todo/Form.vue
+++ src/components/todo/Form.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div(class="card")
     div(class="card-body")
-      form(class="form" @submit.prevent="$emit('submit')")
+      form(class="form mb-0" @submit.prevent="$emit('submit')")
         div(class="form-group")
           input(class="form-control" type="text" v-model="todo.title" placeholder="Title" required)
         div(class="form-group")
@@ -11,12 +11,13 @@
             input(class="form-check-input" type="checkbox" id="done" v-model="todo.done")
             label(class="form-check-label" for="done")
               | Done
-        button(class="btn btn-outline-primary btn-sm btn-block" type="submit")
+        Button(modificators="outline-primary sm block" type="submit")
           | Add
 </template>
 
 <script>
 import getDefaultTodoState from "@/utils/getDefaultTodoState";
+import Button from "@/components/shared/Button";
 
 export default {
   name: "todo-form",
@@ -27,6 +28,9 @@ export default {
         return { ...getDefaultTodoState() };
       }
     }
+  },
+  components: {
+    Button
   }
 };
 </script>
