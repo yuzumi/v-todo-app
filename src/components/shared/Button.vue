06427205ad91ed9c76dcb86a38560defcce036e8
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-27 21:13:02 +0300

    Create the Button component

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  button(:class="finalClass" @click="$emit('click')")
+    slot
+</template>
+
+<script>
+export default {
+  name: "button",
+  props: {
+    modificators: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  computed: {
+    finalClass() {
+      return [
+        "btn",
+        ...this.modificators.split(" ").map(modificator => `btn-${modificator}`)
+      ].join(" ");
+    }
+  }
+};
+</script>
