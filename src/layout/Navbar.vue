39598c7b38538e2912130303bc867156037d9190
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-01 13:09:45 +0300

    Apply LocaleChanger component

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -4,15 +4,19 @@
       i(class="fas fa-home")
     div(class="ml-auto")
       Nav
+    div(class="ml-2")
+      LocaleChanger
 </template>
 
 <script>
 import Nav from "@/layout/Nav";
+import LocaleChanger from "@/internationalization/LocaleChanger";
 
 export default {
   name: "app-navbar",
   components: {
-    Nav
+    Nav,
+    LocaleChanger
   }
 };
 </script>
