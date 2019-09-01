974a37fe73ee7503e06ffa4c25da3603fd74a223
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-01 13:08:38 +0300

    Create the LocaleChanger component

---
+++ src/internationalization/LocaleChanger.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  form(class="form locale-changer")
+    div(class="form-group mb-0")
+      select(class="form-control" v-model="$i18n.locale")
+        option(v-for="language in languages" :key="language" :value="language")
+          | {{language}}
+</template>
+
+<script>
+export default {
+  name: "locale-changer",
+  data() {
+    return {
+      languages: ["en", "ua"]
+    };
+  }
+};
+</script>
