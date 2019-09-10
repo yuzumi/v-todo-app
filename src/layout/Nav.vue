6ac23fc1d8135dd5b9a019c551532354bb3689be
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-10 21:41:20 +0300

    Refactor

--- src/layout/Nav.vue
+++ src/layout/Nav.vue
@@ -1,13 +1,35 @@
 <template lang="pug">
   nav(class="nav")
-    router-link(class="nav-link text-white" to="/")
-      i(class="fas fa-pen")
-    router-link(class="nav-link text-white" to="/new")
-      i(class="fas fa-plus")
+    router-link(
+      class="nav-link text-white"
+      v-for="({ to, icon }) in links"
+      :key="to"
+      :to="to"
+    )
+      Icon(:name="icon")
 </template>
 
 <script>
+import Icon from "@/components/shared/Icon";
+
 export default {
-  name: "app-nav"
+  name: "app-nav",
+  data() {
+    return {
+      links: [
+        {
+          to: "/",
+          icon: "pen"
+        },
+        {
+          to: "/new",
+          icon: "plus"
+        }
+      ]
+    };
+  },
+  components: {
+    Icon
+  }
 };
 </script>
--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   nav(class="navbar navbar-dark bg-primary")
     router-link(class="navbar-brand" to="/")
-      i(class="fas fa-home")
+      Icon(name="home")
     div(class="ml-auto")
       Nav
     div(class="ml-2")
@@ -10,12 +10,14 @@
 
 <script>
 import Nav from "@/layout/Nav";
+import Icon from "@/components/shared/Icon";
 import LocaleChanger from "@/internationalization/LocaleChanger";
 
 export default {
   name: "app-navbar",
   components: {
     Nav,
+    Icon,
     LocaleChanger
   }
 };
