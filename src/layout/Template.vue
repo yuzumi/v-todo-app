5ebe8bb3e94f4ea8d225cac83befd60b2ce01bb9
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-10 21:46:59 +0300

    Implement a smooth transition between pages

--- src/layout/Template.vue
+++ src/layout/Template.vue
@@ -7,7 +7,8 @@
     main(class="main")
       div(class="container pt-5")
         div(class="row")
-          router-view
+          transition(name="fade" mode="out-in")
+            router-view
     //- Footer
     footer(class="footer")
 </template>
@@ -22,3 +23,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
